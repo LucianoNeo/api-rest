bed05c0957987501612e33cbcfd4e8581a70e4c1
LucianoNeo <tec.info.luciano@hotmail.com>
2022-09-08 14:25:13 -0300

    basic patch and delete added

--- routes/users.js
+++ routes/users.js
@@ -13,5 +13,33 @@ router.post('/',(req,res,next) =>{
     })
 })
 
+router.get('/:user_id',(req,res,next) =>{
+    const id = req.params.user_id
+
+    if(id === 'special'){
+        res.status(200).send({
+            message: 'Special ID discovered',
+            id:id
+        })
+    }else{
+        res.status(200).send({
+            message: 'Usual ID discovered',
+            id:id
+        })
+    }
+})
+
+router.patch('/',(req,res,next) =>{
+    res.status(201).send({
+        message: 'Using the PATCH on users route'
+    })
+})
+
+router.delete('/',(req,res,next) =>{
+    res.status(201).send({
+        message: 'Using the DELETE on users route'
+    })
+})
+
 
 module.exports = router
