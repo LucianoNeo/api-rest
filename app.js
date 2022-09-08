12068bedda618c6f0722791fd4ae6aa379b09aba
LucianoNeo <tec.info.luciano@hotmail.com>
2022-09-08 14:39:45 -0300

    error handling

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 const express = require('express')
 
+const morgan = require('morgan')
+
 const app = express()
 
 const usersRoute = require('./routes/users')
 
+
+app.use(morgan('dev')) // faz o log das requests
+
+
+
 app.use('/users', usersRoute)
 
 
+app.use((req,res,next)=>{
+    const error = new Error('Route not found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500)
+    return res.send({
+        error:{
+            message: error.message
+        }
+    })
+})
+
 module.exports= app
+
+
+
+
+
+
+
+
+
+
